2b4c261ed30b5b5c159d9bc36a9d1c1559bedca5
Shubham Prakash Prasad <70102028+iamCynoMan@users.noreply.github.com>
2021-05-28 20:57:02 +0530

    add py Ques.2

--- task.py
+++ task.py
@@ -11,3 +11,20 @@ a = input("Enter First Number: ")
 b = input("Enter 2nd Number: ")
 c = input("Enter 3rd Number: ")
 ques1(a, b, c)
+
+
+# # # WAP to find the number of notes (Sample of notes: 10, 20, 50, 100, 200 and 500 ) against a given amount.
+
+# def ques2(a):
+#     Q = [500, 200, 100, 50, 20, 10]
+#     x = 0
+#     for i in range(6):
+#         q = Q[i]
+#         x += int(a / q)
+#         a = int(a % q)
+#     if a > 0:
+#         x = -1
+#     return print(x)
+
+
+# ques2(int(input()))
